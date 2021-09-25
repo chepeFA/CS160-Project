8f9bc0bf1bc8a1cacffee81ac36640717ba1548c
Francisco <francisco07arias@gmail.com>
2021-09-24 18:19:19 -0700

    testing nd & f

--- TestSim.py
+++ TestSim.py
@@ -141,13 +141,13 @@ def main():
     s.addChannel(s.FLOODING_CHANNEL);
 
     #s.runTime(20);
-    #s.runTime(20);
+    s.runTime(20);
     #floding
-    #s.ping(1, 5, "Hello world");
+    s.ping(1, 5, "Hello world");
     s.runTime(5);
     #testing neighboor discovery
-    s.neighborDMP(6);#list of neighbor for node 4
-    s.runTime(5);
+    #s.neighborDMP(6);#list of neighbor for node 4
+    #s.runTime(5);
    # s.neighborDMP(3);#list of neighbor for node 1
     #s.ping(1, 3, "Hi");
     #s.runTime(10);
