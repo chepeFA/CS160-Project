7e3bccb74452372f25b4d281841bd68280e8c6cd
Francisco <francisco07arias@gmail.com>
2021-09-24 18:17:59 -0700

    testing nd & f

--- TestSim.py
+++ TestSim.py
@@ -146,7 +146,7 @@ def main():
     #s.ping(1, 5, "Hello world");
     s.runTime(5);
     #testing neighboor discovery
-    s.neighborDMP(8);#list of neighbor for node 4
+    s.neighborDMP(6);#list of neighbor for node 4
     s.runTime(5);
    # s.neighborDMP(3);#list of neighbor for node 1
     #s.ping(1, 3, "Hi");
