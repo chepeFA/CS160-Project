1d1f1dd3caf5b48fc8b01f8da89134d3e995e5ef
chepeFA <francisco07arias@gmail.com>
2021-10-13 11:35:23 -0700

    2nd pj

--- TestSim.py
+++ TestSim.py
@@ -142,7 +142,7 @@ def main():
     s.addChannel(s.ROUTING_CHANNEL);
 
     #s.runTime(20);
-    s.runTime(10);
+    s.runTime(20);
     #floding
     s.ping(1, 4, "Hello world");
     s.runTime(5);
