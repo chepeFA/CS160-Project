004c78baa79a9f3a5b67395f63750bb5222c05cd
chepeFA <francisco07arias@gmail.com>
2021-10-17 23:15:12 -0700

    do

--- TestSim.py
+++ TestSim.py
@@ -147,32 +147,32 @@ def main():
     s.ping(1, 8, "Hello world");
     s.runTime(20);
     #testing neighboor discovery
-    s.neighborDMP(1);
-    s.runTime(5);
+    #s.neighborDMP(1);
+    #s.runTime(5);
 
-    s.neighborDMP(2);
-    s.runTime(5);
+    #s.neighborDMP(2);
+    #s.runTime(5);
 
-    s.neighborDMP(3);
-    s.runTime(5);
+    #s.neighborDMP(3);
+    #s.runTime(5);
 
-    s.neighborDMP(4);
-    s.runTime(5);
+    #s.neighborDMP(4);
+    #s.runTime(5);
 
-    s.neighborDMP(5);
-    s.runTime(5);
+    #s.neighborDMP(5);
+    #s.runTime(5);
 
-    s.neighborDMP(6);
-    s.runTime(5);
+    #s.neighborDMP(6);
+    #s.runTime(5);
 
-    s.neighborDMP(7);
-    s.runTime(5);
+    #s.neighborDMP(7);
+    #s.runTime(5);
 
-    s.neighborDMP(8);
-    s.runTime(5);
+    #s.neighborDMP(8);
+    #s.runTime(5);
 
-    s.neighborDMP(9);
-    s.runTime(5);
+    #s.neighborDMP(9);
+    #s.runTime(5);
     #s.neighborDMP(2);#list of neighbor for node 4
     #s.runTime(1);
 
@@ -185,8 +185,8 @@ def main():
     #s.routeDMP(2);
     #s.runTime(5);
 
-    #s.routeDMP(3);
-    #s.runTime(5);
+    s.routeDMP(3);
+    s.runTime(5);
 
     #s.routeDMP(4);
     #s.runTime(5);
