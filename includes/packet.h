5c9e681b3485ebb235ea427db53ad5dc8e2ebae8
chepeFA <francisco07arias@gmail.com>
2021-10-28 22:50:52 -0700

    3rd pj

--- includes/packet.h
+++ includes/packet.h
@@ -25,6 +25,19 @@ typedef nx_struct pack{
 	nx_uint8_t payload[PACKET_MAX_PAYLOAD_SIZE];
 }pack;
 
+typedef nx_struct TCP_Pack{
+
+	nx_uint16_t destPort;
+	nx_uint16_t srcPort;
+	nx_uint16_t seq;
+	nx_uint16_t lastAcked;
+	nx_uint8_t ACK;
+	nx_uint8_t flag;
+	nx_uint8_t window;
+	nx_uint16_t payload[6];
+
+}TCP_Pack
+
 /*
  * logPack
  * 	Sends packet information to the general channel.
